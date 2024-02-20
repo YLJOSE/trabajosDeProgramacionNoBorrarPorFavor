73236026708e281c4f70a48672f42b96fb34d342
YLJOSE <joserojasr47@gmail.com>
2024-02-20 20:49:05 +0100

    fin fibonacci

--- SegundoTrimestreJuanJo/src/main/java/metodosRecursivos/Fibonacci.java
+++ SegundoTrimestreJuanJo/src/main/java/metodosRecursivos/Fibonacci.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 
 public class Fibonacci {
 	public static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("ingresa el tamaño:");
-		int size= sc.nextInt();
+		int size = sc.nextInt();
 		recorrer(size);
-		
+
 	}
+
 	static void recorrer(int number) {
-		int ayuda=0;
-	int anterior=0;
+		int num1 = 0, num2 = 1, suma = 1;
+
 		for (int i = 1; i <= number; i++) {
-			ayuda=ayuda + anterior;
-			System.out.print(ayuda);
-			anterior =i;
-		
-			
+			System.out.print(suma);
+
+			// primero sumamos
+			suma = num1 + num2;
+			// guarda el numero anterior
+			num1 = num2;
+			// se guarda el numero siguiente
+			num2 = suma;
+
 		}
-		
-		
+
 	}
 
 }
